fabbdd455b6bd1620d6173687967d1eea485307e
ADAWAS <pasitsupholkhan@gmail.com>
2022-11-21 21:20:03 +0700

    add pickle

--- PyModelDeployment/medml.py
+++ PyModelDeployment/medml.py
@@ -1,17 +1,16 @@
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
 
 
 class ai:
     def __init__(self, data, features, target, test_size):
         self.X = data.loc[:, features].values
-        self.y = data.loc[features].values.ravel()
-        # self.model = self.
+        self.y = data[target].values.ravel()
+        self.model = self.learn(self.X, self.y, test_size)
 
     def learn(self, X, y, test_size):
         X_train, X_test, y_train, y_test = train_test_split(
             X, y, test_size=test_size, stratify=y)
-        clf = LinearRegression(max_iter=10000, penalty='L2')
+        clf = LogisticRegression(max_iter=10000, penalty='l2')
         clf.fit(X_train, y_train)
         return clf
-
--- PyModelDeployment/mymodel.py
+++ PyModelDeployment/mymodel.py
@@ -1,6 +1,53 @@
+import pickle
 import pandas as pd
-import numpy as np
+# import numpy as np
+import medml as ml
 
-df = pd.read_csv("./data/processed.cleveland.data")
+column_names = ["age", "sex", "cp", "trestbps", "chol", "fbs",
+                "restecg", "thalach", "exang", "oldpeak", "slope",
+                "ca", "thal", "num"]
+# use fullpath for url
+url = 'C:/Users/ADAWAS/Desktop/repos/R_CEB/PyModelDeployment/data/processed.cleveland.data'
+df = pd.read_csv(url, header=None, names=column_names)
+
+df = df[(df['thal'] != '?') & (df['ca'] != '?')].reset_index(drop=True)
 
 
+df["Labels"] = df['num'].apply(lambda x: 0 if x == 0 else 1)
+
+features = ['thal', 'exang', 'cp', 'ca', 'slope']
+
+# thal
+thal = pd.get_dummies(df["thal"])
+thal.columns = ["normal", "fixed defect", "reversable defect"]
+df = pd.concat([df, thal], axis=1)
+
+# cp
+df['cp'] = df['cp'].map(
+    {1: "typical angina", 2: "atypical angina", 3: "non-anginal pain", 4: "asymptomatic"})
+cp = pd.get_dummies(df['cp'])
+df = pd.concat([df, cp], axis=1)
+
+# slope
+df['slope'] = df['slope'].map(
+    {1: "upsloping", 2: "flat", 3: "downsloping"})
+slope = pd.get_dummies(df['slope'])
+df = pd.concat([df, slope], axis=1)
+
+select_features = ["normal", "fixed defect", "reversable defect",
+                   "typical angina",  "atypical angina",  "non-anginal pain",  "asymptomatic",
+                   "upsloping", "flat", "downsloping", "exang", "ca", "Labels"]
+
+data = df.loc[:, select_features]
+
+features = data.columns.tolist()
+features.remove('Labels')
+
+# print(df)
+
+heart = ml.ai(data=df, features=features, target='Labels', test_size=0.2)
+
+# Serialize your model
+print(heart.model.coef_)
+
+pickle.dump(heart.model, open('Logreghearth.pkl', 'wb'))
