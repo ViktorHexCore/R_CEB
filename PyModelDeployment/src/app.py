a24885d211afe104ed3d46bf3ee3be133997a396
ADAWAS <pasitsupholkhan@gmail.com>
2022-11-21 21:43:49 +0700

    use rout for flask

--- PyModelDeployment/src/app.py
+++ PyModelDeployment/src/app.py
@@ -1,10 +1,36 @@
 # save this as app.py
 from flask import Flask
+import pickle
+
+myheart = pickle.load(
+    open('./PyModelDeployment/model/Logreghearth.pkl', 'rb'))
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-    return "Hello, World!"
-if(__name__=='__main__'):
-    app.run(debug=True)
+    return '''
+    <body>
+        <h2>Hello, World!</h2>
+    </body>
+    '''
+    # return "Hello, World!"
+
+
+@app.route("/greet/<name>")
+def greet(name):
+    return "Hello, {}!".format(name)
+
+
+@app.route("/modelcoef")
+def show_model_coef():
+    # print("Coef load:", myheart.coef_)
+    return "Model Coefficients"
+    # + myheart.coef_
+
+
+if(__name__ == '__main__'):
+    HOST = '127.0.0.1'
+    PORT = '5000'
+    app.run(HOST, PORT)
